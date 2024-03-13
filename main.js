82dfd9bee34ba9897cfb56f0caf0ad0394fb39e6
Dmitry <free-doom46@mail.ru>
2024-03-13 13:09:04 +0300

    feat: add msg filter

--- main.js
+++ main.js
@@ -1,24 +1,110 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import dotenv from "dotenv";
+dotenv.config();
+import { Bot, GrammyError, HttpError } from "grammy";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const BOT_KEY = process.env.BOT_API_KEY;
+const bot = new Bot(BOT_KEY);
 
-setupCounter(document.querySelector('#counter'))
+// Команда, принимает первым аргументом название команды
+// Вторым асинхронную функцию с действием
+// Слушаем ввод
+
+const Person = {
+  dub: {
+    birthday: { 
+      date: "01.03.1994"
+    },
+    location: {
+      address: "Чистова 15.15 кв.62",
+    },
+  },
+};
+
+bot.api.setMyCommands([
+  {
+    command: "start",
+    description: "Запускает бота",
+  },
+  {
+    command: "help",
+    description: "Помогите",
+  },
+  {
+    command: "birthday",
+    description: "Узнать др",
+  },
+  {
+    command: "location",
+    description: "Узнать адрес",
+  },
+]);
+
+// command = ответ на команды [или массив команд] вида 'start' (или любое другое значение)
+bot.command("start", async (ctx) => {
+  await ctx.reply(`Привет ${ctx.from.first_name}`);
+});
+
+// bot.command("location", async (ctx) => {
+//   await ctx.reply(`Вот тебе адрес человека: ${Person.dub.location.address}`);
+// });
+
+
+// //? кастомная фильтрация
+// bot.on('msg').filter((ctx) => {
+//   return ctx.from.id === 280181578;
+// }, async (ctx) => {
+//   await ctx.reply(`Привет: ${ctx.from.id}`)
+// })
+
+// // фильтрация по доке
+// bot.on("message", async (ctx) => {
+//   await ctx.reply(`Сообщение: `);
+// });
+
+
+// слушатель конкретных сообщений [или массива], может принимать первым аргументом expReg 
+bot.hears('жопа', async (ctx) => {
+  await ctx.reply(`Сам ты жопа`)
+})
+
+// пример проверки слов через expReg
+bot.hears(/хуй/, async (ctx) => {
+  await ctx.reply(`ругаешься?`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// обработка ошибок
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
+bot.start();
