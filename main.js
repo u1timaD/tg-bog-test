6b1ae5548863b849a5314fd6afec2b6cd2302096
Dmitry <free-doom46@mail.ru>
2024-03-14 18:20:10 +0300

    feat: add btn address

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { Bot, GrammyError, HttpError, Keyboard} from "grammy";
+import { Bot, GrammyError, HttpError, Keyboard } from "grammy";
 
 const BOT_KEY = process.env.BOT_API_KEY;
 const bot = new Bot(BOT_KEY);
@@ -9,128 +9,79 @@ const bot = new Bot(BOT_KEY);
 // Вторым асинхронную функцию с действием
 // Слушаем ввод
 
-const Person = {
-  dub: {
-    birthday: { 
-      date: "01.03.1994"
-    },
-    location: {
-      address: "Чистова 15.15 кв.62",
-    },
-  },
-};
 
-bot.api.setMyCommands([
-  {
-    command: "start",
-    description: "Запускает бота",
-  },
-  {
-    command: "mood",
-    description: "Помогите",
-  },
-  {
-    command: "birthday",
-    description: "Узнать др",
-  },
-  {
-    command: "location",
-    description: "Узнать адрес",
-  },
-]);
+// ?Тут вставить данные 
 
-// command = ответ на команды [или массив команд] вида 'start' (или любое другое значение)
+// bot.api.setMyCommands([
+//   {
+//     command: "start",
+//     description: "Запуск бота",
+//   },
+//   {
+//     command: "info",
+//     description: "Что может бот",
+//   },
+// ]);
+
 bot.command("start", async (ctx) => {
-  await ctx.reply(`Привет ${ctx.from.first_name}`);
+  const firstKey = new Keyboard().text("Адрес").text("ДР").resized();
+
+  await ctx.reply(`Привет <b>${ctx.from.username}</b>. Что ты хочешь узнать?`, {
+    reply_markup: firstKey,
+    parse_mode: "HTML",
+  });
 });
 
-//? можно сделать клавиатуру 
-//? ontime() - сворачивает клавиатуру (также можно убрать через колбек)
-//?  reply_markup: {remove_keyboard: true} - совсем убирает клаву
-//? resized() - подгоняет кнопки под размер содержимого
-bot.command('mood', async (ctx) => {
-  const moodKeyboard = new Keyboard().text('Хорошо').row().text('Норм').text('Плохо').resized()
+bot.command("info", async (ctx) => {
+  const firstKey = new Keyboard().text("Адрес").text("ДР").resized();
 
-  // ? тоже самое, только через массив значений 
-  // const moodLabels = ['Хор', 'Отл', 'Пло'];
-  // const rows = moodLabels.map((label) => {
-  //   return [
-  //     Keyboard.text(label)
-  //   ]
-  // })
-  // const moodKeyboard2 = Keyboard.from(rows).resized()
+  await ctx.reply(
+    `Привет *${ctx.from.username}*
+Этот бот может не так много, лишь рассказать тебе о том где живут твои друзья, либо о том, когда у них др
 
-  await ctx.reply('Как настроение?', {
-    reply_markup: moodKeyboard
-  })
-})
+Спасибо тебе друг`,
+    {
+      reply_markup: firstKey,
+      parse_mode: "MarkdownV2",
+    }
+  );
+});
 
-bot.hears('Хорошо', async (ctx) => {
-  const key2 = new Keyboard().text('точно?').text('Пизжжю').resized()
-  await ctx.reply('Точно всё хорошо?',{
-    reply_markup: key2
-  })
-})
+bot.hears("Адрес", async (ctx) => {
+  const addressKey = new Keyboard().resized().row();
 
+  Friends.forEach((friend) => {
+    addressKey.text(friend.family);
+  });
 
-// bot.hears('Хорошо', async (ctx) => {
-//   await ctx.reply('Отлично', {
-//     reply_markup: {remove_keyboard: true}
-//   })
-// })
+  await ctx.reply("Чей адрес тебя интересует?", {
+    reply_markup: addressKey,
+  });
+});
 
-// ? Можно запрашивать в консоли инфу
-// bot.on('msg', async (ctx) => {
-//   console.log(ctx.me)
-// })
+// Получения локации в зависимости от человека
+const getPersonLocation = (name) => {
+  const { address, floor, room, code, entry } = Friends.filter(
+    (item) => item.family === `${name}`
+  )[0].location;
+  return `*Адрес:* ${address}
+*этаж:* ${floor}
+*квартира:* ${room}
+*домофон:* ${code}`;
+};
 
-// bot.command("location", async (ctx) => {
-//   await ctx.reply(`Вот тебе адрес человека: ${Person.dub.location.address}`);
-// });
+bot.hears(SecondName, async (ctx) => {
+  const personBtn = new Keyboard().text("Назад").text("Узнать Др").resized();
 
+  await ctx.reply(getPersonLocation(ctx.match), {
+    reply_markup: personBtn,
+    parse_mode: "MarkdownV2",
+  });
+});
 
-// //? кастомная фильтрация
-// bot.on('msg').filter((ctx) => {
-//   return ctx.from.id === 280181578;
-// }, async (ctx) => {
-//   await ctx.reply(`Привет: ${ctx.from.id}`)
-// })
-
-//? фильтрация по доке
-// bot.on("message", async (ctx) => {
-//   await ctx.reply(`Сообщение: `);
-// });
-
-
-//? слушатель конкретных сообщений [или массива], может принимать первым аргументом expReg 
-bot.hears('жопа', async (ctx) => {
-  await ctx.reply(`Сам ты жопа`)
-})
-
-//? Можно добавить "ответ" на сообщение добавлением 2 параметра в reply
-//? Через parse_mode можно добавлять стили 
-//? также можно добавлять реакции на сообщения
-bot.hears('пидор', async (ctx) => {
-  await ctx.react('🌚');
-  await ctx.reply(`сам ты <b>${ctx.msg.text}</b>`, {
-    reply_parameters: { message_id: ctx.msg.message_id},
-    parse_mode: 'HTML'
-  })
-
-})
-
-// пример проверки слов через expReg
-bot.hears(/хуй/, async (ctx) => {
-  await ctx.reply(`сам ты ${ctx.msg.text}`)
-})
-
-
-
-
-
-
-
-
+bot.on("message", async (ctx) => {
+  await ctx.reply("Я тебя не понимаю, пожалуйста выбери одну из команд");
+});
 
 
 
